43f6e86cf39e077f6332c157cfe0d0687f6dcf6b
Manikanta Reddy <manikantaareddy12@gmail.com>
2023-06-12 15:50:41 +0530

    Initial commit

--- smsandemailsendingapp/models.py
+++ smsandemailsendingapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class RegModels(models.Model):
+    FName=models.CharField(max_length=20)
+    LName=models.CharField(max_length=20)
+    UserName=models.CharField(max_length=20)
+    Password=models.CharField(max_length=10)
+    CPassword=models.CharField(max_length=10)
+    Emailid=models.EmailField()
+    MobileNo=models.CharField(max_length=13)
